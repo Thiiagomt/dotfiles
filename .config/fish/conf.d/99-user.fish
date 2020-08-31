a97bee0b4bd7d51a8fe9ad128d87b69112d12475
Thiago <tmoraes505@gmail.com>
2020-08-30 23:46:46 -0300

    update

--- .config/fish/conf.d/99-user.fish
+++ .config/fish/conf.d/99-user.fish
@@ -1,8 +1,4 @@
-alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
-
 alias :q=exit
-alias aget='aria2c -s 16 -x 16 -j 16 -k 1M'
-alias set-vulkan-intel='export VK_ICD_FILENAMES=/usr/share/vulkan/icd.d/intel_icd.x86_64.json'
 
 set fish_greeting "Lansa a braba"
 set -gx PATH $PATH ~/.local/bin
